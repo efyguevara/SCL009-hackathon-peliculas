7f181c198fc569b3d112f3e01bd34dc3c844931a
efyguevara <efyguevara@gmail.com>
2019-05-21 22:44:42 -0400

    Agregando rated, organizacion del html para los select, elimino codigo muerto, se dejan todas las funciones en el main. Se declara la funcion movieDetails fuera del onload para acceder a ella desde todas las cards.

--- src/main.js
+++ src/main.js
@@ -1,23 +1,27 @@
+//aqui se imprimen las cards
+const list = document.getElementById('movies');
 //Recarga la pagina
 const home = document.getElementById("home");
 home.addEventListener("click", () => {
     location.reload(true);
 })
 
-//Click del boton "buscar"
-const clickSearch = document.getElementById("clickSearcher");
+function addListenerButtonDetails() {
+    const buttons = document.querySelectorAll('.btn-details');//con este querysSelector se hace el evento click del boton
+    for (const button of buttons) { //recorre cada boton 
+        button.addEventListener('click', movieDetails);
+    }
+}
 
-//Cuando escuche el click va a llamar a la data para buscar lo que el usurio ingreso en el input seach
-clickSearch.addEventListener("click", () => {
-    //texto ingresado por el usuario (titulo de pelicula)
-    const inputSearch = document.getElementById("searcher").value;
+//esta funcion muestra el detalle de la pelicula seleccionada
+function movieDetails(event) {
+    console.log('entre!');
     //parametros para armar la url
-    const params = { api_key: "879f4d45aca2ee6235c83898a8eb220c", query: inputSearch, language: 'es-ES' };
+    const movieId = event.target.getAttribute("data-id");//este atributo me da el valor del ID y lo toma como parametro para la url
+    const params = { api_key: "879f4d45aca2ee6235c83898a8eb220c", language: 'es-ES', external_source: "imdb_id" };
     const urlParams = new URLSearchParams(Object.entries(params));
-
-    const list = document.getElementById('movies');
     //llamada a la data con los parametros que se establecieron en URLSearchParams
-    fetch(`https://api.themoviedb.org/3/search/movie?${urlParams}`)
+    fetch(`https://api.themoviedb.org/3/find/${movieId}?${urlParams}`)
         .then((response) => {
             return response.json();
         })
@@ -41,5 +45,144 @@ clickSearch.addEventListener("click", () => {
                 button.addEventListener('click', movieDetails);
             }
         })
-});
+}
+window.onload = () => {
 
+    //llamada a la data con los parametros que se establecieron en URLSearchParams
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=879f4d45aca2ee6235c83898a8eb220c&sort_by=popularity.desc`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            list.innerHTML = "";
+            data.results.forEach((element) => {
+                list.innerHTML +=
+                    `<div class="col-sm-12 col-md-4 col-lg-2">
+                        <div class="card text-center cards">
+                            <img src="https://image.tmdb.org/t/p/w500/${element.poster_path}" class="card-img-top"  alt="${element.original_title} Imágen no Disponible">
+                            <div class="card-body">
+                                <p class="card-title ">${element.title}</p>
+                            </div>
+                            <button id="e-${element.id}" data-id="${element.id}" class="btn btns btn-lg btn-block btn-details   ">Ver más</button>
+                        </div>
+                    </div>`;
+            });
+            addListenerButtonDetails();
+        })
+
+    //Click del boton "buscar"
+    const clickSearch = document.getElementById("clickSearcher");
+    //Cuando escuche el click va a llamar a la data para buscar lo que el usurio ingreso en el input seach
+    clickSearch.addEventListener("click", () => {
+        //texto ingresado por el usuario (titulo de pelicula)
+        const inputSearch = document.getElementById("searcher").value;
+        //parametros para armar la url
+        const params = { apikey: "376741b9", s: inputSearch, plot: "full" };
+        const urlParams = new URLSearchParams(Object.entries(params));
+
+
+        //llamada a la data con los parametros que se establecieron en URLSearchParams
+        fetch(`https://www.omdbapi.com?${urlParams}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                list.innerHTML = "";
+                data.Search.forEach((element) => {
+                    //console.log(data)
+                    list.innerHTML +=
+                        `<div class="col-sm-12 col-md-4 col-lg-2">
+                    <div class="card text-center cards">
+                        <img src="${element.Poster}" class="card-img-top"  alt="${element.Title} Imágen no Disponible">
+                        <div class="card-body">
+                            <p class="card-title ">${element.Title}</p>
+                        </div>
+                        <button id="e-${element.imdbID}" data-id="${element.imdbID}" class="btn btns btn-lg btn-block btn-details   ">Ver más</button>
+                    </div>
+                </div>`;
+                });
+                addListenerButtonDetails();
+
+            })
+    });
+
+    /*Filtro por género*/
+    const filterMovie = document.getElementById("genre-movie");
+    filterMovie.addEventListener("change", () => {
+        const filterMovie = document.getElementById("genre-movie").value;
+        //console.log(filterMovie)
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=879f4d45aca2ee6235c83898a8eb220c&with_genres=" + filterMovie + "&sort_by=popularity.desc&language=es-ES")
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                list.innerHTML = "";
+                data.results.forEach((element) => { 
+                    list.innerHTML +=
+                        `<div class="col-sm-12 col-md-4 col-lg-2">
+                    <div class="card text-center cards">
+                        <img src="https://image.tmdb.org/t/p/w500/${element.poster_path}" class="card-img-top"  alt="${element.original_title} Imágen no Disponible">
+                        <div class="card-body">
+                            <p class="card-title ">${element.title}</p>
+                        </div>
+                        <button id="e-${element.id}" data-id="${element.id}" class="btn btns btn-lg btn-block btn-details   ">Ver más</button>
+                    </div>
+                </div>`;
+                });
+                addListenerButtonDetails();
+            });
+    })
+
+    /*Filtro por año*/
+    const yearMovie = document.getElementById("year-movie");
+    yearMovie.addEventListener("change", () => {
+        const yearMovie = document.getElementById("year-movie").value;
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=879f4d45aca2ee6235c83898a8eb220c&primary_release_year=" + yearMovie)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                list.innerHTML = "";
+                data.results.forEach((element) => {
+                    console.log(data)
+                    list.innerHTML +=
+                        `<div class="col-sm-12 col-md-4 col-lg-2">
+                    <div class="card text-center cards">
+                        <img src="https://image.tmdb.org/t/p/w500/${element.poster_path}" class="card-img-top"  alt="${element.original_title} Imágen no Disponible">
+                        <div class="card-body">
+                            <p class="card-title ">${element.title}</p>
+                        </div>
+                        <button id="e-${element.id}" data-id="${element.id}" class="btn btns btn-lg btn-block btn-details   ">Ver más</button>
+                    </div>
+                </div>`;
+                });
+                addListenerButtonDetails();
+            });
+    })
+
+    /*Filtro por Top Rated*/
+    const ratedMovie = document.getElementById("rated-movie");
+    ratedMovie.addEventListener("change", () => {
+        const ratedMovie = document.getElementById("rated-movie").value;
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=879f4d45aca2ee6235c83898a8eb220c&with_genres=16,12,10751,35&sort_by=vote_average." + ratedMovie)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                list.innerHTML = "";
+                data.results.forEach((element) => {
+                    list.innerHTML +=
+                    `<div class="col-sm-12 col-md-4 col-lg-2">
+                        <div class="card text-center cards">
+                            <img src="https://image.tmdb.org/t/p/w500/${element.poster_path}" class="card-img-top"  alt="${element.original_title} Imágen no Disponible">
+                            <div class="card-body">
+                                <p class="card-title ">${element.title}</p>
+                            </div>
+                            <button id="e-${element.id}" data-id="${element.id}" class="btn btns btn-lg btn-block btn-details">Ver más</button>
+                        </div>
+                    </div>`;
+                });
+                addListenerButtonDetails();
+            });
+    })
+}
