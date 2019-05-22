2e9f762e792230fc91ad72d3a91bec69d537b97e
Stefany Guevara <efyguevara@gmail.com>
2019-05-22 15:14:59 -0400

    Resolucion de conflictos, validator ok.

--- src/main.js
+++ src/main.js
@@ -27,19 +27,21 @@ function movieDetails(event) {
         })
         .then((data) => {
             list.innerHTML = "";
-            list.classList.add("movie-container")
             data.results.forEach((element) => {
-                console.log(data)
+                let poster = element.poster_path
+                let imageUrl = `https://image.tmdb.org/t/p/w500/${poster}`
+                if (element.poster_path === null) {
+                    //alert('holiii')
+                    element.poster_path = imageUrl = 'img/no.png'
+                }
+
                 list.innerHTML +=
-                    `<div class="card col-sm-12 col-md-4 col-lg-2 text-center cards">
-                        <img src="https://image.tmdb.org/t/p/w500/${element.poster_path}" class="card-img-top" " alt="${element.original_title} Imágen no Disponible">
-                        <div class="card-body">
-                            <h5 class="card-title">${element.title}</h5>
-                        </div>
-                        <div class="row text-center">
-                            <button id="e-${element.id}" data-id="${element.id}" class="btn btn-lg btn-block btns btn-details">Ver más</button>
-                        </div>
-                    </div>`;
+                    `<div class="col-sm-12 col-md-4 col-lg-2">
+                <div class="card text-center cards">
+                    <img src="${imageUrl}" class="card-img-top"  alt="${element.original_title} Imágen no Disponible">
+                    <button id="e-${element.id}" data-id="${element.id}" class="btn btns btn-lg btn-block btn-details">Ver más</button>
+                </div>
+            </div>`;
             });
             const buttons = document.querySelectorAll('.btn-details');
             for (const button of buttons) {
@@ -56,18 +58,21 @@ window.onload = () => {
         })
         .then((data) => {
             list.innerHTML = "";
-            list.classList.add("movie-container")
             data.results.forEach((element) => {
+                let poster = element.poster_path
+                let imageUrl = `https://image.tmdb.org/t/p/w500/${poster}`
+                if (element.poster_path === null) {
+                    //alert('holiii')
+                    element.poster_path = imageUrl = 'img/no.png'
+                }
+
                 list.innerHTML +=
                     `<div class="col-sm-12 col-md-4 col-lg-2">
-                        <div class="card text-center cards">
-                            <img src="https://image.tmdb.org/t/p/w500/${element.poster_path}" class="card-img-top"  alt="${element.original_title} Imágen no Disponible">
-                            <div class="card-body">
-                                <p class="card-title ">${element.title}</p>
-                            </div>
-                            <button id="e-${element.id}" data-id="${element.id}" class="btn btns btn-lg btn-block btn-details   ">Ver más</button>
-                        </div>
-                    </div>`;
+                <div class="card text-center cards">
+                    <img src="${imageUrl}" class="card-img-top"  alt="${element.original_title} Imágen no Disponible">
+                    <button id="e-${element.id}" data-id="${element.id}" class="btn btns btn-lg btn-block btn-details">Ver más</button>
+                </div>
+            </div>`;
             });
             addListenerButtonDetails();
         })
@@ -81,8 +86,6 @@ window.onload = () => {
         //parametros para armar la url
         const params = { apikey: "376741b9", s: inputSearch, plot: "full" };
         const urlParams = new URLSearchParams(Object.entries(params));
-
-
         //llamada a la data con los parametros que se establecieron en URLSearchParams
         fetch(`https://www.omdbapi.com?${urlParams}`)
             .then((response) => {
@@ -90,103 +93,74 @@ window.onload = () => {
             })
             .then((data) => {
                 list.innerHTML = "";
-                list.classList.add("movie-container")
                 data.Search.forEach((element) => {
-                    //console.log(data)
+                    if (element.Poster === 'N/A') {
+                        element.Poster = 'img/no.png'
+                    }
                     list.innerHTML +=
                         `<div class="col-sm-12 col-md-4 col-lg-2">
                     <div class="card text-center cards">
                         <img src="${element.Poster}" class="card-img-top"  alt="${element.Title} Imágen no Disponible">
-                        <div class="card-body">
-                            <p class="card-title ">${element.Title}</p>
-                        </div>
+                        
                         <button id="e-${element.imdbID}" data-id="${element.imdbID}" class="btn btns btn-lg btn-block btn-details   ">Ver más</button>
                     </div>
                 </div>`;
                 });
                 addListenerButtonDetails();
-
             })
     });
 
     /*Filtro por género*/
     const filterMovie = document.getElementById("genre-movie");
     filterMovie.addEventListener("change", () => {
-        const filterMovie = document.getElementById("genre-movie").value;
-        //console.log(filterMovie)
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=879f4d45aca2ee6235c83898a8eb220c&with_genres=" + filterMovie + "&sort_by=popularity.desc&language=es-ES")
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                list.innerHTML = "";
-                 list.classList.add("movie-container")
-                data.results.forEach((element) => { 
-                    list.innerHTML +=
-                        `<div class="col-sm-12 col-md-4 col-lg-2">
-                    <div class="card text-center cards">
-                        <img src="https://image.tmdb.org/t/p/w500/${element.poster_path}" class="card-img-top"  alt="${element.original_title} Imágen no Disponible">
-                        <div class="card-body">
-                            <p class="card-title ">${element.title}</p>
-                        </div>
-                        <button id="e-${element.id}" data-id="${element.id}" class="btn btns btn-lg btn-block btn-details   ">Ver más</button>
-                    </div>
-                </div>`;
-                });
-                addListenerButtonDetails();
-            });
+        filterMovies();
     })
 
     /*Filtro por año*/
     const yearMovie = document.getElementById("year-movie");
     yearMovie.addEventListener("change", () => {
-        const yearMovie = document.getElementById("year-movie").value;
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=879f4d45aca2ee6235c83898a8eb220c&primary_release_year=" + yearMovie)
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                list.innerHTML = "";
-                data.results.forEach((element) => {
-                    console.log(data)
-                    list.innerHTML +=
-                        `<div class="col-sm-12 col-md-4 col-lg-2">
-                    <div class="card text-center cards">
-                        <img src="https://image.tmdb.org/t/p/w500/${element.poster_path}" class="card-img-top"  alt="${element.original_title} Imágen no Disponible">
-                        <div class="card-body">
-                            <p class="card-title ">${element.title}</p>
-                        </div>
-                        <button id="e-${element.id}" data-id="${element.id}" class="btn btns btn-lg btn-block btn-details   ">Ver más</button>
-                    </div>
-                </div>`;
-                });
-                addListenerButtonDetails();
-            });
+        filterMovies();
     })
 
     /*Filtro por Top Rated*/
     const ratedMovie = document.getElementById("rated-movie");
     ratedMovie.addEventListener("change", () => {
+        filterMovies();
+    })
+
+    function filterMovies() {
         const ratedMovie = document.getElementById("rated-movie").value;
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=879f4d45aca2ee6235c83898a8eb220c&with_genres=16,12,10751,35&sort_by=vote_average." + ratedMovie)
+        const yearMovie = document.getElementById("year-movie").value;
+        const filterMovie = document.getElementById("genre-movie").value;
+
+        const params = {
+            api_key: "879f4d45aca2ee6235c83898a8eb220c", with_genres: filterMovie,
+            sort_by: 'vote_average.' + ratedMovie, primary_release_year: yearMovie, language: 'es-ES'
+        };
+        const urlParams = new URLSearchParams(Object.entries(params));
+        fetch(`https://api.themoviedb.org/3/discover/movie?${urlParams}`)
             .then((response) => {
                 return response.json();
             })
             .then((data) => {
                 list.innerHTML = "";
                 data.results.forEach((element) => {
+                    let poster = element.poster_path
+                    let imageUrl = `https://image.tmdb.org/t/p/w500/${poster}`
+                    if (element.poster_path === null) {
+                        //alert('holiii')
+                        element.poster_path = imageUrl = 'img/no.png'
+                    }
+
                     list.innerHTML +=
-                    `<div class="col-sm-12 col-md-4 col-lg-2">
-                        <div class="card text-center cards">
-                            <img src="https://image.tmdb.org/t/p/w500/${element.poster_path}" class="card-img-top"  alt="${element.original_title} Imágen no Disponible">
-                            <div class="card-body">
-                                <p class="card-title ">${element.title}</p>
-                            </div>
-                            <button id="e-${element.id}" data-id="${element.id}" class="btn btns btn-lg btn-block btn-details">Ver más</button>
-                        </div>
-                    </div>`;
+                        `<div class="col-sm-12 col-md-4 col-lg-2">
+                    <div class="card text-center cards">
+                        <img src="${imageUrl}" class="card-img-top"  alt="${element.original_title} Imágen no Disponible">
+                        <button id="e-${element.id}" data-id="${element.id}" class="btn btns btn-lg btn-block btn-details">Ver más</button>
+                    </div>
+                </div>`;
                 });
                 addListenerButtonDetails();
             });
-    })
+    }
 }
